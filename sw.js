9112f4a71529348dcf8ca6f81f1c9e6ce2540cab
Maverick-GTO <irusanch@gmail.com>
2026-01-05 14:04:55 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,10 +1,7 @@
-const cacheName = 'kroatisch-v6';
-const assets = ['./', './index.html', './manifest.json', './icon.png'];
-
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(cacheName).then(cache => cache.addAll(assets)));
+const cacheName = 'kroatisch-v7';
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(cacheName).then((cache) => cache.addAll(['./', './index.html', './manifest.json', './icon.png'])));
 });
-
-self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+self.addEventListener('fetch', (e) => {
+  e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
 });
