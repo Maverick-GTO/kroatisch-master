c3d0d7d260be127fff8f108c8690f956007df678
Maverick-GTO <irusanch@gmail.com>
2026-01-05 14:07:03 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,41 @@
-const cacheName = 'kroatisch-v7';
-self.addEventListener('install', (e) => {
-  e.waitUntil(caches.open(cacheName).then((cache) => cache.addAll(['./', './index.html', './manifest.json', './icon.png'])));
+/* Service Worker für Kroatisch Master */
+const cacheName = 'kroatisch-v8';
+const assets = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon.png'
+];
+
+// Installieren des Service Workers und Speichern der Dateien im Cache
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(cacheName).then(cache => {
+      console.log('Service Worker: Dateien werden gecacht');
+      return cache.addAll(assets);
+    })
+  );
 });
-self.addEventListener('fetch', (e) => {
-  e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
+
+// Aktivieren und Löschen alter Cache-Versionen
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keyList => {
+      return Promise.all(keyList.map(key => {
+        if (key !== cacheName) {
+          console.log('Service Worker: Alter Cache wird gelöscht', key);
+          return caches.delete(key);
+        }
+      }));
+    })
+  );
+});
+
+// Abrufen der Dateien (zuerst aus dem Cache, dann aus dem Netzwerk)
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(res => {
+      return res || fetch(e.request);
+    })
+  );
 });
